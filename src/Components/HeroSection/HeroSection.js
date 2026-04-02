3f3fc073df6276ef65204b67ab90cd0514863bf2
fadyashraf8 <fadyashraf909@gmail.com>
2026-04-02 18:07:03 +0200

    hero section edit

--- src/Components/HeroSection/HeroSection.js
+++ src/Components/HeroSection/HeroSection.js
@@ -22,37 +22,37 @@ const NeonHighlight = ({ text }) => (
 export default function HeroSection({ HeroData, ClientLogos }) {
   return (
     <>
-      <div className="grid grid-cols-2">
-        <div className="flex flex-col justify-evenly">
-          <div>
+      <div
+        className="grid grid-cols-2 relative min-h-screen"
+        style={{
+          backgroundImage: "url('/HeroSection/Car.png')",
+          backgroundSize: "cover",
+          backgroundPosition: "20% center",
+          backgroundRepeat: "no-repeat",
+        }}
+      >
+        <div className="flex flex-col justify-evenly relative z-10 bg-[#03031D]">
+
+          <div  className="p-5">
             <h1 className="hero-heading">
               A Performance Driven advertising Agency on the
-            
               <NeonHighlight text="Fastest Line to Growth" />
             </h1>
-            <p className="text-[#D7D8DB] font-[400] text-[1.5rem]">Racing Line helps brands compete, adapt, and win in fast-moving markets.</p>
+            <p className="text-[#D7D8DB] font-[400] text-[1.5rem]">
+              Racing Line helps brands compete, adapt, and win in fast-moving markets.
+            </p>
           </div>
 
-          <div className="grid grid-cols-4 gap-2 mt-5">
+          <div className="grid grid-cols-4 gap-2 mt-5 w-[120%] p-5 bg-[#03031D] rounded-tr-[20px] rounded-br-[20px]" >
             <CardCategory SrcPage={`/services/digitalMarketing`} text={"Digital Marketing"} bgImage={"/OurServices/ourservices4.png"} />
             <CardCategory SrcPage={`/services/digitalMarketing`} text={"Offline Marketing"} bgImage={"/OurServices/ourservices4.png"} />
             <CardCategory SrcPage={`/services/digitalMarketing`} text={"Event Management"} bgImage={"/OurServices/ourservices4.png"} />
             <CardCategory SrcPage={`/services/digitalMarketing`} text={"Web Development"} bgImage={"/OurServices/ourservices4.png"} />
           </div>
         </div>
-
-        <div>
-          <Image
-            src={'/HeroSection/Car.png'}
-            alt={HeroData?.image?.alt || "Hero Image"}
-            width={600}
-            height={400}
-            className="w-full h-auto object-cover"
-          />
-        </div>
       </div>
 
-      <ScrollingLogos logos={ClientLogos} />
+      {/* <ScrollingLogos logos={ClientLogos} /> */}
     </>
   );
 }
