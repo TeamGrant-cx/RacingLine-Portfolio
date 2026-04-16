3d694d37165d5a4f450cfaa2d0da980e9318f7be
fadyashraf8 <fadyashraf909@gmail.com>
2026-04-16 11:16:25 +0200

    hero section edit

--- src/Components/CardCategory/CardCategory.js
+++ src/Components/CardCategory/CardCategory.js
@@ -6,12 +6,12 @@ export default function CardCategory({ SrcPage, text, bgImage, withHoverEffect =
   return (
     <Link
       href={SrcPage}
-      className={`border-gradient-color 
-       rounded-[1.25rem] py-[1.25rem] ${withHoverEffect ? 'group' : ''}`}
+      className={`border-gradient-color
+       rounded-[1.25rem] aspect-[1/1.15] flex flex-col justify-between p-[1rem] ${withHoverEffect ? 'group' : ''}`}
 
     >
 
-      <div className="relative w-full aspect-[389/280] overflow-visible">
+      <div className="relative w-full flex-1 overflow-visible">
         <Image
           className={`absolute -top-8 left-1/2 -translate-x-1/2 w-[80%] h-auto ${
             withHoverEffect
@@ -26,11 +26,11 @@ export default function CardCategory({ SrcPage, text, bgImage, withHoverEffect =
       </div>
 
 
-      <div className="flex  items-center justify-around  text-white pt-[2.5rem] px-[1rem]">
+      <div className="flex items-center justify-between text-white pt-[0.5rem]">
 
         <h1 className="text-white text-[1.25rem] font-[500]">{text}</h1>
 
-        <i className="text-[2.3rem] rotate-45 fa-solid fa-chevron-right arrow-icon-glow bg--500" ></i>
+        <i className="text-[2.3rem] rotate-45 fa-solid fa-chevron-right arrow-icon-glow" ></i>
 
       </div>
 
