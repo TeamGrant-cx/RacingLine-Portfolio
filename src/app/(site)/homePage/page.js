02dc365420661bb6f3931a1cd943a2a8a86524d8
fadyashraf8 <fadyashraf909@gmail.com>
2026-04-27 16:02:21 +0300

    clean code

--- src/app/(site)/homePage/page.js
+++ src/app/(site)/homePage/page.js
@@ -1,28 +1,12 @@
 import ClientsTea from '@/Components/ClientsTea/ClientsTea';
-import ClientsTeaServer from '@/Components/ClientsTea/ClientsTeaServer'
 import HeroSection from '@/Components/HeroSection/HeroSection'
 import OurServices from '@/Components/OurServices/OurServices';
-import OurServicesServer from '@/Components/OurServices/OurServicesServer'
 import RaceLineSuccess from '@/Components/RaceLineSuccess/RaceLineSuccess';
 import React from 'react'
 
-async function getHomePageData() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_SITE_URL}/api/globals/home-page`, {
-    cache: "no-store",
-  });
-  if (!res.ok) return null;
-  return res.json();
-}
+
 
 export default async function HomePage() {
-  // const data = await getHomePageData();
-
-
-    
-  // const HeroData = data?.hero ?? {};
-  // const ClientLogos = data?.clientLogos ?? [];
-  // const RaceLineSuccessData = data?.raceLineSuccess ?? {};
-  // const ClientsTea = data?.clientsTea ?? {};
 
   return (
     <>
@@ -30,7 +14,6 @@ export default async function HomePage() {
         <HeroSection  />
         <RaceLineSuccess  />
         <OurServices />
-        {/* <ServicesGridFigma /> */}
       </div>
   <ClientsTea />
     </>
